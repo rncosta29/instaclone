2c4a24f67a134b87f93d48cbbbb2e4928e7d6142
Rafael Costa <rafael.costa@yaman.com.br>
2020-04-24 09:59:55 -0300

    Rotas com guardas - idToken ainda nao esta ok

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Usuario } from './acesso/usuario.model';
 import * as firebase from 'firebase';
 
+@Injectable()
 export class Autenticacao {
+
+    public token_id: string;
+
+    constructor(
+        private router: Router
+    ) { }
 
     public cadastrarUsuario(usuario: Usuario): Promise<any> {
         return firebase.auth()
@@ -21,15 +30,38 @@ export class Autenticacao {
     }
 
     public autenticar(email: string, senha: string): void {
-        console.log('email', email);
-        console.log('senha', senha);
         firebase.auth()
             .signInWithEmailAndPassword(email, senha)
             .then((resposta: any) => {
-                console.log(resposta);
+                firebase.auth().currentUser.getIdToken()
+                    .then((idToken: string) => {
+                        this.token_id = idToken;
+                        localStorage.setItem('idToken', idToken)
+                        this.router.navigate(['home']);
+                    })
             })
             .catch((error: Error) => {
                 console.log(error);
+            })
+    }
+
+    public autenticado(): boolean {
+        if(this.token_id === undefined && localStorage.getItem('idToken')) {
+            this.token_id = localStorage.getItem('idToken');
+        }
+        if(this.token_id === undefined) {
+            this.router.navigate(['/']);
+        }
+        return this.token_id !== undefined;
+    }
+
+    public sair(): void {
+        firebase.auth()
+            .signOut()
+            .then(() => {
+                localStorage.removeItem('idToken');
+                this.token_id = undefined;
+                this.router.navigate(['/']);
             })
     }
 }
